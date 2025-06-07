0bb924d6f0e8cdd843fd243705b5c8a910583429
replakcan <alper.makcan@gmail.com>
2025-06-07 13:42:14 +0300

    Checks if capacity increase logic works fine

--- main.js
+++ main.js
@@ -1,43 +1,5 @@
 import HashMap from './HashMap.js'
 
-/* const map = new HashMap()
-
-console.log(map)
-map.set('alper', 'mutlu')
-map.set('alper', 'zortingen')
-map.set('alper', 'cafu')
-map.set('b', 'o')
-map.set('c', 'simdi ')
-map.set('d', 'asker')
-map.set('e', 'neler')
-map.set('f', 'neler')
-map.set('g', 'ister')
-map.set('h', 'beberuhi')
-
-console.log(map.hash('alper'))
-console.log(map)
-console.log(map.buckets[2].headNode)
-
-console.log(map.get('b'))
-console.log(map.get('alper'))
-
-console.log(map.has('alper'))
-console.log(map.has('b'))
-console.log(map.has('testingo'))
-
-console.log(map.buckets[2].headNode)
-console.log(map.buckets[3].headNode)
-console.log(map.buckets[4].headNode)
-console.log(map.buckets[5].headNode)
-console.log(map.buckets[6].headNode)
-console.log(map.buckets[7].headNode)
-
-console.log(map.length())
-
-console.log(map.keys())
-console.log(map.values())
-console.log(map.entries()) */
-
 const test = new HashMap()
 
 test.set('apple', 'red')
@@ -53,9 +15,12 @@ test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
 
-test.set('lion', 'definetlyNotGolden')
-test.set('hat', 'white')
+console.log(test.entries())
 
-console.log(test.keys())
-console.log(test.values())
+test.set('alper', 'golden')
+test.set('mutlu', 'golden')
+test.set('sadomazo', 'golden')
+test.set('qwer', 'golden')
+test.set('y', 'golden')
+
 console.log(test.entries())
